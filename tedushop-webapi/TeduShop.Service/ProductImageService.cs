5c966f910a26f692e71ffd3c431a56b1ae48ee98
LeChiLuc <leluc276@gmail.com>
2017-12-10 16:27:19 +0700

    add project tedushop-webapi

---
+++ tedushop-webapi/TeduShop.Service/ProductImageService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Data.Repositories;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Service
+{
+    public interface IProductImageService
+    {
+        void Add(ProductImage productImage);
+
+        void Delete(int id);
+
+        void Update(ProductImage productImage);
+
+        ProductImage GetById(int id);
+
+        List<ProductImage> GetAll(int productId);
+
+        void Save();
+    }
+
+    public class ProductImageService : IProductImageService
+    {
+        private IProductImageRepository _productImageRepository;
+        private IUnitOfWork _unitOfWork;
+
+        public ProductImageService(IProductImageRepository productImageRepository, IUnitOfWork unitOfWork)
+        {
+            this._productImageRepository = productImageRepository;
+            this._unitOfWork = unitOfWork;
+        }
+
+        public void Add(ProductImage productImage)
+        {
+            _productImageRepository.Add(productImage);
+        }
+
+        public void Delete(int id)
+        {
+            _productImageRepository.Delete(id);
+        }
+
+        public void Update(ProductImage productImage)
+        {
+            _productImageRepository.Update(productImage);
+        }
+
+        public ProductImage GetById(int id)
+        {
+            return _productImageRepository.GetSingleById(id);
+        }
+
+        public List<ProductImage> GetAll(int productId)
+        {
+            return _productImageRepository.GetMulti(x => x.ProductId == productId).ToList();
+        }
+
+        public void Save()
+        {
+            _unitOfWork.Commit();
+        }
+    }
+}
---
+++ tedushop-webapi/TeduShop.WebAPI/Controllers/testController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Mvc;
+using TeduShop.Service;
+using TeduShop.Web.Infrastructure.Core;
+
+namespace TeduShop.Web.Controllers
+{
+    public class test
+    {
+        public test(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+
+
+    [RoutePrefix("api/test")]
+    public class testController : ApiControllerBase
+    {
+
+        public testController(IErrorService errorService) : base(errorService)
+        {
+
+        }
+
+        [Route("getall")]
+        [HttpGet]
+        public HttpResponseMessage getAll(HttpRequestMessage request)
+        {
+            HttpResponseMessage response = null;
+            Func<HttpResponseMessage> func = () =>
+            {
+                var lst = new List<test>() {
+                    new test(1,"abc"),
+                    new test(2,"abc"),
+                    new test(3,"abc"),
+                    new test(4,"abc"),
+                    new test(5,"abc")
+                };
+                response = request.CreateResponse(HttpStatusCode.OK, lst);
+                return response;
+            };
+            return CreateHttpResponse(request, func);
+        }
+    }
+}
